009313f0e4d3c4dbee44d18c6ed4598e3aabe663
Taihoon Kim <kishu@navercorp.com>
2017-04-26 17:45:57 +0900

    chore(config): update configuration files and typos

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,24 @@
 module.exports = function(config) {
 	var karmaConfig = {
-		frameworks: ['mocha', 'chai', 'sinon'],
+		frameworks: ["mocha", "chai", "sinon"],
 
 		files: [
 			"./test/unit/*.tmpl.html",
-			'./node_modules/phantomjs-polyfill/bind-polyfill.js',
-			'./node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
-			'./node_modules/iscroll/build/iscroll.js',
-			'./test/unit/*.spec.js'
+			"./node_modules/phantomjs-polyfill/bind-polyfill.js",
+			"./node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js",
+			"./node_modules/iscroll/build/iscroll.js",
+			"./test/unit/*.spec.js"
 		],
 
 		// karma을 실행할 때, 아래 패턴은 위에서 설정한 webpack설정.
 		preprocessors: {
 			"./test/unit/*.tmpl.html": ["html2js"],
-			'./test/unit/*.spec.js': ['webpack']
+			"./test/unit/*.spec.js": ["webpack"]
 		},
 
 		// webpack 설정
 		webpack: {
-			devtool: 'inline-source-map',
+			devtool: "inline-source-map",
 			module: {
 				rules: [
 					{
@@ -42,13 +42,13 @@ module.exports = function(config) {
 
 
 		// 리포트 타입(mocha)
-		reporters: ['mocha'],
+		reporters: ["mocha"],
 
 		// 브라우저 설정
 		browsers: ["PhantomJS_custom"],
 		customLaunchers: {
-			'PhantomJS_custom': {
-				base: 'PhantomJS',
+			"PhantomJS_custom": {
+				base: "PhantomJS",
 				options: {
 					viewportSize: {
 						width: 1024,
@@ -70,18 +70,18 @@ module.exports = function(config) {
 
 	// coverage을 설정한 경우
 	if(config.coverage) {
-		karmaConfig.preprocessors['./test/**/*.spec.js'].push('sourcemap');
-		karmaConfig.reporters.push('coverage-istanbul');
+		karmaConfig.preprocessors["./test/**/*.spec.js"].push("sourcemap");
+		karmaConfig.reporters.push("coverage-istanbul");
 		// text랑 html로 리포트
 		karmaConfig.coverageIstanbulReporter = {
-			reports: [ 'text-summary' , 'html'],
-			dir: './coverage'
+			reports: [ "text-summary" , "html"],
+			dir: "./coverage"
 		};
 		// coverage의 순서을 위로
 		karmaConfig.webpack.module.rules.unshift({
 			test: /\.js$/,
 			exclude: /(node_modules|test)/,
-			loader: 'istanbul-instrumenter-loader'
+			loader: "istanbul-instrumenter-loader"
 		});
 		karmaConfig.singleRun = true;
 	}
