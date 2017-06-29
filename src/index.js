a202fcc1bf7733a61cc99fdcbcdd4042ba2136f5
Son Chan Uk <sculove@gmail.com>
2017-06-29 10:12:46 +0900

    check env configs (#1)
    * test(all): fix it
    feat(index): add version
    chore(package): add es2015
    
    * chore(.npmignore): update npmignore
    
    * fix(demo): fix bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Visible from "./visible";
 
+Visible.VERSION = "#__VERSION__#";
 module.exports = Visible;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var webpack = require("webpack");
 var merge = require("webpack-merge");
+var pkg = require("./package.json");
 var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
-
+var StringReplacePlugin = require("string-replace-webpack-plugin");
 var path = require("path");
 var parts = require("./webpack.parts");
 
@@ -12,28 +13,48 @@ var config = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "[name].js",
-		library: ["eg", "Visible"],
-		libraryTarget: "umd",
-		umdNamedDefine: true
+		library: [pkg.namespace.eg, "Visible"],
+		libraryTarget: "umd"
 	},
 	externals: {
-		"@egjs/component" : {
-			root: ["eg", "Component"]
+		"@egjs/component": {
+			root: [pkg.namespace.eg, "Component"]
 		}
 	},
 	module: {
 		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "babel-loader",
-			options: {
-				presets: [
-					["es2015", { modules: false }]
-				]
+				test: /(\.js)$/,
+				exclude: /(node_modules)/,
+				loader: "babel-loader",
+				query: {
+					"presets": [ 
+						[
+							"es2015",
+							{
+								"loose": true,
+							}
+						]
+					],
+					"plugins": [
+						"add-module-exports"
+					]
+				}
+			},
+			{
+				test: /(\.js)$/,
+				loader: StringReplacePlugin.replace({
+					replacements: [{
+						pattern: /#__VERSION__#/ig,
+						replacement: function (match, p1, offset, string) {
+							return pkg.version;
+						}
+					}]
+				})
 			}
-		}]
+		]
 	},
 	plugins: [
+		new StringReplacePlugin(),
 		new UglifyJSPlugin({
 			include: /\.min\.js$/,
 			beautify: false,
@@ -54,7 +75,7 @@ var config = {
 	]
 };
 
-module.exports = function(env) {
+module.exports = function (env) {
 	env = env || "development";
 
 	if (env === "development") {
